d89a339dd157eabf922b0d87b52f2f1497c73f29
bashi <bashi57.d@gmail.com>
2022-06-19 21:33:48 +0900

    質問する

--- 004/004_WA.rb
+++ 004/004_WA.rb
@@ -1,14 +1,25 @@
+# aが変わったから、aとbは変わっているのに、なんでcとdは変わっていないの?
+# メソッドならokなの？
 h,w = gets.split.map(&:to_i)
 a = Array.new(h) {gets.split.map(&:to_i)}
 
-a_trans = a.transpose
-
-answer = a.each_with_index {|row,i|
+b = a+[0]
+c = a.map(&:sum)
+d = a.map {_1+[1]}
+# a_trans = a.transpose.map(&:sum)
+answer = a.each {|row|
     row_sum = row.sum
-
-    row.map! {|x|
-        x = row_sum + a_trans[i].sum - x
-    }
+    row.map! {|x| x = row_sum - x}
 }
+p a
+p b
+p c
+p d
+# p answer
+# p a_trans
 
-p answer
+# answer = (answer.transpose).each_with_index {|row,i|
+#     row.map! {|x| x += a_trans[i]}
+# }
+
+# (answer.transpose).each {puts _1.join(" ")}
